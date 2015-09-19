e61862f5ab0ea56db477a27100910f87152e3b3e
Andrew Paradi <andrew@v1021-wn-11-59.campus-dynamic.uwaterloo.ca>
2015-09-19 15:35:27 -0400

    Code cleanup

--- maps/maps.js
+++ maps/maps.js
@@ -1,21 +1,4 @@
-// variables
-
-
-
-// When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
-
-var list01 = {
-	'name': 'iPhone 5S 64GB',
-	'price': '$80',
-	'desc': 'BNIB Proin magna mauris, fringilla at. Ut volutpat ut risus non vulputate. Donec luctus erat dolor, sit amet scelerisque purus blandit sit amet.',
-	'img': 'img/iphone.png',
-	'postal': 'N2G1E6',
-	'city': 'Mississauga',
-	'rating': 4.4,
-	'postDate':1442646904,
-	'url': '/1.html'
-	};
 	
 var item1 = {
     "title": "BRAND NEW IPHONEEE OMG. BUY IT PLEASE",
@@ -33,7 +16,6 @@ var item1 = {
     "price": "$80"
 };
 
-
 var item2 = {
     "title": "BRAND NEW XBOX OMG",
     "description": "Sick new iphone 5. Like legit dope. Hit me up a text or email or something. I just want to talk 2 someome. ",
@@ -50,43 +32,8 @@ var item2 = {
     "price": "$80"
 };
 
-	
-console.log(list01.name);
-
-/*
-
-
-
-
-function listing(list01) {
-	return '<div id="htncontent">'+
-      '<div id="htnsiteNotice">'+
-      '</div>'+
-      '<h1 id="htnfirstHeading" class="htnfirstHeading">'+list01.name+'</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>');
-};
-*/
-
-
-
 function init() {
     // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
         zoom: 11,
@@ -124,8 +71,18 @@ function init() {
 	    infoBubble.close();
     });
 
-    // Let's also add a marker while we're at it
+    // Markers
     
+/*
+    var marker2 = new google.maps.Marker({
+        position: new google.maps.LatLng(data.loc.lat, data.loc.lat),
+        map: map,
+        title: 'marker',
+        data: item2
+//         icon: 'http://maps.google.com/mapfiles/
+    });
+    
+*/
     
     
     var marker = new google.maps.Marker({
@@ -149,33 +106,7 @@ function init() {
     
     
     var materialString = '<div class="card">' + '<div class="image">' + '<img src="' + marker.data.pictures.pic2 + '"/>' + '<span id="item-title" class="title">' + marker.data.title + '</span>' + '</div>'  + '<div id="content">' + /* '<div id="item-price" class="item-price">' + marker.data.price + '</div>' + */ '<p id="item-descr" class="content">' + marker.data.description + '</div>' + '<div class="action"> <a href="'+ 'http://www.cnn.com/' + '">Read More</a> <a href="' + 'http://www.cnn.com/' + '">Bookmark</a></div></div>';
-	 
-/*
-  	var infowindow = new google.maps.InfoWindow({
-	    content: materialString,
-	    maxWidth: 500
-		});
-*/
-		
-		
-/*
-		
-		
-	var marker2 = new google.maps.Marker({
-        position: new google.maps.LatLng(40.800, -73.9400),
-        map: map,
-        title: 'marker',
-        data: item
-//         icon: 'http://maps.google.com/mapfiles/
-    });
-    
-    marker.addListener('click', function() {
-	    infowindow.open(map, marker2);
-		}); 
-		
-		
-		
-*/
+
 
 		
 	var infoBubble = new InfoBubble({
@@ -184,7 +115,7 @@ function init() {
       position: (40.800, -73.5400),
       shadowStyle: 0,
       padding: 0,
-      margin: 0,
+      margin: -5,
       backgroundColor: 'rgba(57,57,57, 0.0)',
       borderRadius: 5,
       arrowSize: 10,
@@ -206,16 +137,7 @@ function init() {
 		});
     
     infoBubble.open(map, marker); 
-			
-	
-		
-/*
-	Document.querySelector(#item-cta).addListener('click', goToItem(marker.data.url) {
-		infowindow.close();
-			
-	})
-*/
-		
+
 		
   
     
